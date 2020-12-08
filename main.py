26370b48d14061072ef0d075fbdece7e182eef6f
Isayah Culbertson <isayahculbertson@gmail.com>
2020-12-08 08:41:01 -0500

    testing

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime, timezone, timedelta
 from time import sleep
 
+import logging
+from logging.config import fileConfig
+logger = logging.getLogger()
+
 def get_utc_time() ->datetime.time:
     now_utc_date = datetime.now(timezone.utc)
     now_utc_time = now_utc_date.time() # coverts datetime into time object
@@ -14,21 +18,20 @@ def utc_to_nyc():
     nyc_time = get_utc_time() - timedelta(hours=5)
     return nyc_time
 
-def wait_for_publishing(wait_minutes:int):
-    #inital time is 6.am nyc or 11 utc
-    while not get_today_link():
-        sleep(60*wait_minutes)
+# def wait_for_publishing(wait_minutes:int):
+#     #inital time is 6.am nyc or 11 utc
 
-    current_time = get_utc_time()
-    sched = BlockingScheduler()
-    sched.add_job(send_message(
-        get_today_link()), 
-        'cron', 
-        minute =current_time.minute+1,
-        hour=current_time.hour)
+#     #problematic code
+#     while not get_today_link():
+#         sleep(60*wait_minutes)
 
-def main():
-    wait_for_publishing(15)
+#     current_time = get_utc_time()
+
+
+# def main():
+    
+#     wait_for_publishing(15)
+
 
 #wsb usually publishes around 6am
 #start conquence then
@@ -39,7 +42,14 @@ def main():
 
 
 if __name__=='__main__':
-    main()
+    logging.debug("I'm a message for debugging purposes.")
+    # main()
+    sched = BlockingScheduler()
+    sched.add_job(send_message(
+    get_today_link()), 
+    'cron', 
+    minute =45,
+    hour=13)
 
 
     
